f3b3aff8fde2efde3630ec5f046f1be7c5ca7403
capy8ra <peng33@illinois.edu>
2026-03-23 17:34:49 -0500

    change background color

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import './index.css'
+import theme from './theme'
 import App from './App'
 
-const theme = extendTheme({
-  config: {
-    initialColorMode: 'system',
-    useSystemColorMode: true,
-  },
-})
+// Clear cached color mode so we always follow the system preference
+localStorage.removeItem('chakra-ui-color-mode')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
